4b88de599a0462c8c5f179922b41080eaa5737da
Adrian <Adrian@Mac-Mini-M4---Adrian.local>
2025-12-17 10:50:57 +0100

    fix: Rename internal nginx to odoo-proxy and remove tracked certificates

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo "Syncing files to kooldock.local..."
 rsync -avz --exclude 'node_modules' --exclude '.next' --exclude '.git' --exclude '.DS_Store' . kooldock@kooldock.local:/Users/kooldock/odoo
 
 echo "Deploying to kooldock.local..."
-ssh kooldock@kooldock.local "export PATH=\$PATH:/usr/local/bin && cd /Users/kooldock/odoo && docker compose down && docker compose up -d --build"
+ssh kooldock@kooldock.local "export PATH=\$PATH:/usr/local/bin && export DOCKER_CONFIG=\$(mktemp -d) && mkdir -p \$DOCKER_CONFIG/cli-plugins && ln -s ~/.docker/cli-plugins/* \$DOCKER_CONFIG/cli-plugins/ && echo '{\"credsStore\":\"desktop\"}' > \$DOCKER_CONFIG/config.json && cd /Users/kooldock/odoo && docker pull odoo:latest && docker compose down && docker compose up -d --build"
 
 if [ $? -eq 0 ]; then
   echo "Deployment successful! App should be running at http://kooldock.local:8069"
