a394ab263413fe909984905c31b0bd473ca0542e
Koolbrand <hola@koolbrand.com>
2026-01-14 12:02:58 +0100

    Sync theme_koolbrand_invoice from Coolify production environment

---
+++ extra-addons/theme_koolbrand_invoice/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Theme Koolbrand Invoice',
+    'version': '1.0',
+    'summary': 'Theme for Invoice Report',
+    'depends': ['account'],
+    'data': [
+        'views/report_invoice.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
